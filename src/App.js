50ec980eacc361d2268ed9f0c7268462b204a5a0
Park Sieun <pmj8856@naver.com>
2023-01-30 10:41:46 +0900

    Switch문 적용

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 // common
 import Header from "./components/common/Header";
@@ -17,13 +17,22 @@ import Join from "./components/sub/Join";
 function App() {
   return (
     <>
-      <Header />
-      
-      <Route exact path="/">
-        <Visual />
-        <Content />
-      </Route>
-      
+      {/* 화면에 중첩되는 컴포넌트가 있는 경우 Switch 를 활용한다. */}
+      <Switch>
+        <Route exact path="/">
+          {/* 라우터 값에 따라서 Header props 로 type="main" 을 전달 */}
+          <Header type={"main"} />
+          <Visual />
+          <Content />
+        </Route>
+
+        {/* 라우터에 따라서 Header 의 css 를 달리하겠다. */}
+        {/* 중첩되는 Header 에 대한 처리가 필요하다. */}
+        {/* 컴포넌트를 출력하는 3번째 방법 */}
+        {/* <Header type={"sub"} /> */}
+        <Route path="/" render={() => <Header type={"sub"} />} />
+      </Switch>
+
       <Route path="/department" component={Department} />
       <Route path="/community" component={Community} />
       <Route path="/gallery" component={Gallery} />
