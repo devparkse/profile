d47b35b67bcd53ac038b776a58be3cba8f91e2c6
Park Sieun <pmj8856@naver.com>
2023-02-01 10:19:33 +0900

    useEffect 및 useRef 를 활용한 모션 작업

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -1,18 +1,32 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
-// Layout.js : 공통 요소를 적용하기 위한 처리 컴포넌트
 const Layout = (props) => {
+  // useRef(초기값) 는 변수입니다. 즉, state 가 아닙니다. (useRef 는 html 태그 저장하는 용도로 사용)
+  // useState(초기값) state 는 무엇인가?
+  // 아래 구문은 real DOM 을 참조함. (DOM : html 태그)
+  // 하지만 아직 real DOM 이 안만들어졌으므로
+  // 참조가 어렵다.
+  // useRef(document.querySelector("section"));
+  // 순서 1.
+  const frame = useRef(null);
+
+  useEffect(() => {
+    // frame useRef 를 활용해서 section 태그를 참조해서 css 작업을 하고싶다.
+    frame.current.classList.remove("on");
+    frame.current.classList.add("on");
+    return () => {
+      // 아래 구문은 에러가 발생합니다.
+      // unmount 가 되면 참조 요소가 null 이 된다.
+      // frame.current.classList.remove("");
+    };
+  }, []);
+
   return (
-    // 백틱을 이용해서 Props 로 전달된 값을
-    // 이용해서 ClassName 으로 활용
-    <section className={`content ${props.title}`}>
+    // 순서 2. 상위에 만들어둔 useRef 변수 frame 을 ref 속성으로 참조
+    <section className={`content ${props.title}`} ref={frame}>
       <figure></figure>
       <div className="inner">
         <h1>{props.title}</h1>
-        {/*
-          실제 컴포넌트 내용 
-          props.children 을 이용해서 원하는 영역에 컴포넌트를 배치 가능
-        */}
         {props.children}
       </div>
     </section>
