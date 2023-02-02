5443c5d038801e8d68b374d87a79b0c00a588863
Park Sieun <pmj8856@naver.com>
2023-02-02 10:46:07 +0900

    카카오 지도 1차

--- src/components/sub/Community.js
+++ src/components/sub/Community.js
@@ -1,33 +1,11 @@
 import React, { useEffect } from "react";
 import Layout from "../common/Layout";
 
-// javascript 코드 자리
 const Community = () => {
-  // javascript 코드 자리
-  // 컴포넌트가 마운트, 업데이트(state 변경), 언마운트 시
-  // 하고싶은 일을 작성하는 Hook : useEffect
-  // 기본적 형태
-  // useEffect(() => {
-  //   하고싶은 일
-  //   클린업 함수 : 컴포넌트 제거시 실행
-  //   return () => {
-
-  //   }
-  // }, [의존성배열: dependency Array]);
-
   useEffect(() => {
-    console.log("mount : 컴포넌트 생성");
-    // axios, html 요소 선택하는 작업
-    return () => {
-      console.log("unmount : 컴포넌트 제거");
-    }
+    return () => {};
   }, []);
 
-  // jsx 코드 자리
-  // : 주의사항 return () O
-  // : 주의사항 return {} X
   return <Layout title={"Community"}>Community</Layout>;
 };
-// javascript 코드 자리
-
 export default Community;
